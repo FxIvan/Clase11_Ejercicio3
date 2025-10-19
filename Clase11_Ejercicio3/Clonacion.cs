b1453a2352388e1399f85446cb866eb1835b3ba0
Ivan Lopez <ialmendra@infobae.com>
2025-10-19 20:07:47 -0300

    Agregar archivos de proyecto.

---
+++ Clase11_Ejercicio3/Clonacion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clase11_Ejercicio3
+{
+    public class Municipalidad : ICloneable
+    {
+        public string NombreMunicipalidad { get; set; }
+
+        public object Clone()
+        {
+            // Devuelve una copia superficial de Municipalidad (suficiente, ya que solo tiene tipos primitivos)
+            return this.MemberwiseClone();
+        }
+    }
+
+    public class Empleado : ICloneable
+    {
+        public string Provincia { get; set; }
+        public Municipalidad Ciudad { get; set; }
+
+        // 🔸 Clonación superficial
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        // 🔹 Clonación profunda
+        public Empleado CloneDeep()
+        {
+            // Primero clonamos el empleado
+            Empleado clon = (Empleado)this.MemberwiseClone();
+
+            // Luego clonamos el objeto interno (Ciudad)
+            if (this.Ciudad != null)
+            {
+                clon.Ciudad = (Municipalidad)this.Ciudad.Clone();
+            }
+
+            return clon;
+        }
+    }
+}
---
+++ Clase11_Ejercicio3/Program.cs
@@ -0,0 +1,38 @@
+﻿using Clase11_Ejercicio3;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Empleado Clonacion1 = new Empleado()
+        {
+            Provincia = "Buenos Aires",
+            Ciudad = new Municipalidad { NombreMunicipalidad = "La Plata" }
+        };
+
+        // Clon superficial
+        Empleado Clonacion2 = (Empleado)Clonacion1.Clone();
+
+        // Clon profundo
+        Empleado Clonacion3 = Clonacion1.CloneDeep();
+
+        // Modificamos las propiedades de los clones
+        Clonacion2.Ciudad.NombreMunicipalidad = "Berazategui";
+        Clonacion3.Ciudad.NombreMunicipalidad = "Mar del Plata";
+
+        Console.WriteLine("Original:");
+        Console.WriteLine($"Provincia: {Clonacion1.Provincia}");
+        Console.WriteLine($"Ciudad: {Clonacion1.Ciudad.NombreMunicipalidad}");
+        Console.WriteLine("---------------------------------------");
+
+        Console.WriteLine("Clon superficial:");
+        Console.WriteLine($"Provincia: {Clonacion2.Provincia}");
+        Console.WriteLine($"Ciudad: {Clonacion2.Ciudad.NombreMunicipalidad}");
+        Console.WriteLine("---------------------------------------");
+
+        Console.WriteLine("Clon profundo:");
+        Console.WriteLine($"Provincia: {Clonacion3.Provincia}");
+        Console.WriteLine($"Ciudad: {Clonacion3.Ciudad.NombreMunicipalidad}");
+    }
+}
